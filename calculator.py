259282e3d79bcd6afdb9995579c4395708b230ff
Ryan Mangeno <ryanmangeno@icloud.com>
2023-05-13 13:57:37 -0400

    main

--- calculator.py
+++ calculator.py
@@ -20,45 +20,37 @@ print ('1. Add')
 print ('2. Subtract')
 print ('3. Divide')
 print ('4. Multiply')
+
 #loop to ask for new calc after one is made
-while True:
-    inp = input('Would you like to add, subtract, divide, or multiply two numbers?')
-    inp = inp.lower()
-
 #promt a value
-#if a number is not entered, invalid input is returned
-#same for the second value
 
-    a = input ('what is the first value?')
-    b = input('what is your next value?')
+while True:
+    inp = input('Add(1) Subtract (2) Divide (3) Multiply(4)?')
+    
+#if a the input is not a num then return invalid input and break
     try:
-        a=float(a)
-        b=float(b)
+        a = float(input ('what is the first value?'))
+        b = float(input('what is your next value?'))
     except:
-        print ('Invalid Input')
+        print ('Choose A Valid Number')
         break
 
-#if the input can be converted to an interger, invalid input is returned
-#if it can't, except is ran and the input checks if add, sub, etc. is entered, if not , 'not a valid option' is returne
-#defied functions are used to calculate a and b. End loop if an invalid input is entered
+#if the inp is not 1 , 2 , 3 , 4 invalid is returned
 
-    try:
-        int(inp)
-        print ('Invalid Input')
+    if inp == '1':
+        print (a, '+', b, '=', add(a,b))
+    elif inp == '2':
+        print (a, '-', b, '=', sub(a,b))
+    elif inp == '3':
+        print (a, '/', b, '=', div(a,b))
+    elif inp == '4':
+        print (a, '*', b, '=', mult(a,b))
+    else:
+        print ('not a valid option')
         break
-    except:
-        if inp == 'add':
-            print (a, '+', b, '=', add(a,b))
-        elif inp == 'subtract':
-            print (a, '-', b, '=', sub(a,b))
-        elif inp == 'divide':
-            print (a, '/', b, '=', div(a,b))
-        elif inp == 'multiply':
-            print (a, '*', b, '=', mult(a,b))
-        else:
-            print ('not a valid option')
-            break
-#ask for new calc, if yes new calc, if n or invalid input end loop
+
+#ask for new calc, if yes (y) new calc, if no (n) or invalid input end loop
+    
     next_calc = input ('New Calculation? Y/N')
     next_calc = next_calc.lower()
     if next_calc == 'y':
@@ -66,5 +58,5 @@ while True:
     if next_calc == 'n':
         break
     else:
-        print ('invalid input, you did not input Y,y or N,n')
+        print ('invalid input')
         break
